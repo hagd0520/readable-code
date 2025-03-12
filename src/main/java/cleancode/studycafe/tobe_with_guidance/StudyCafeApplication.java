4fb9c0d0a5d98c85e666129edb94317986f1f3fa
hagd <hagd0520@gmail.com>
2025-03-12 14:06:20 +0900

    [6-4] 리팩토링(3) - FileHandler를 Provider 개념으로 리팩토링

--- src/main/java/cleancode/studycafe/tobe_with_guidance/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/tobe_with_guidance/StudyCafeApplication.java
@@ -1,9 +1,19 @@
 package cleancode.studycafe.tobe_with_guidance;
 
+import cleancode.studycafe.tobe_with_guidance.io.provider.LockerPassFileReader;
+import cleancode.studycafe.tobe_with_guidance.io.provider.SeatPassFileReader;
+import cleancode.studycafe.tobe_with_guidance.provider.LockerPassProvider;
+import cleancode.studycafe.tobe_with_guidance.provider.SeatPassProvider;
+
 public class StudyCafeApplication {
 
     public static void main(String[] args) {
-        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        SeatPassProvider seatPassProvider = new SeatPassFileReader();
+        LockerPassProvider lockerPassProvider = new LockerPassFileReader();
+
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine(
+            seatPassProvider, lockerPassProvider
+        );
         studyCafePassMachine.run();
     }
 
---
+++ src/main/java/cleancode/studycafe/tobe_with_guidance/provider/SeatPassProvider.java
@@ -0,0 +1,8 @@
+package cleancode.studycafe.tobe_with_guidance.provider;
+
+import cleancode.studycafe.tobe_with_guidance.model.pass.StudyCafeSeatPasses;
+
+public interface SeatPassProvider {
+
+    StudyCafeSeatPasses getSeatPasses();
+}
